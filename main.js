082bd3272aa82b081116ab98cae247cf50006e4f
lessthanseventy <andrew@lessthanseventy.com>
2020-01-02 02:49:12 -0500

    fixed styling and added reset animation to play function

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-let elem = document.getElementById("innerContainer");
-
 let play = function() {
-  let topAmount = elem.scrollHeight;
-  elem.style.top = '-' + topAmount + 'px';
+
+  let outputDiv = document.getElementById("innerContainer");
+  let crawlText = document.getElementById("crawlText").value
+
+  outputDiv.classList.add('notransition'); // Disable transitions
+  outputDiv.style.top = '70%'; // Reset output position offscreen
+  outputDiv.offsetHeight; // Trigger a reflow, flushing the CSS changes
+  outputDiv.classList.remove('notransition'); // Re-enable transitions
+
+  if (!crawlText) {
+    crawlText = document.getElementById("crawlText").placeholder;
+  }
+  outputDiv.innerHTML = crawlText;
+
+  let topAmount = outputDiv.offsetHeight;
+  console.log(topAmount);
+  outputDiv.style.top = '-' + topAmount + 'px';
 }
