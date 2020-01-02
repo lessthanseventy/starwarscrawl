4be0e46259ca93a10cfcbcd3e4d72d18d28caa2f
lessthanseventy <andrew@lessthanseventy.com>
2020-01-02 04:07:49 -0500

    fix textarea line breaks

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+var textAreas = document.getElementsByTagName('textarea');
+
+Array.prototype.forEach.call(textAreas, function(elem) {
+    elem.placeholder = elem.placeholder.replace(/\\n/g, '\n');
+});
+
 let play = function() {
 
   let outputDiv = document.getElementById("innerContainer");
